1fef444e576f7b10159e7cafc8132326c8de35a0
Manon <manon.vdm@hotmail.com>
2023-08-10 14:16:31 +0200

    ProductController has been added

--- src/public/db/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-$pdo = new PDO(
-    'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_DATABASE'),
-    getenv('DB_USERNAME'),
-    getenv('DB_PASSWORD')
-);
--- src/public/product.php
+++ src/public/product.php
@@ -1,30 +1,9 @@
 <?php
 declare(strict_types=1);
 
+require 'public/controllers/ProductController.php';
+
 session_start();
 
-try {
-    // 1 - Connexion à la DB
-    require_once 'public/db/connection.php';
-
-    // 2 - Récupérer le produit
-
-    $stmt = $pdo->prepare("SELECT * FROM products WHERE productCode = :code");
-    $stmt->bindParam(":code", $_GET['productCode']);
-    $stmt->execute();
-
-    $product = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if (empty($product)) {
-        echo '404 - no product found';
-        die;
-    }
-
-    // 3 - Afficher la page
-    include 'public/views/layout/header.view.php';
-    include 'public/views/product.view.php';
-    include 'public/views/layout/footer.view.php';
-
-} catch (Exception $e) {
-    print_r($e->getMessage());
-}
+$productController = new ProductController(); 
+$productController->show($_GET['productCode']);
